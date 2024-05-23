14d253f3c97796a10328804138f1b8e5ca397027
Rahul <golirahul21@gmail.com>
2024-05-23 21:45:13 +0530

    NotificationTicket model created

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 Welcome to NOTIFICATION SERVICE
+
+ npx sequelize model:generate --name NotificationTicket --attributes subject:string,content:string,recepientEmail:string,status:enum,notificationTime:date
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,5 +1,8 @@
 const sender = require('../config/email-config')
 
+// This mail sevice need not be await because we dont need immediatley but not too lately
+//******** we can we NODE-CORN/NODE-SHEDULER npm package to send emails at particular time *******
+
 const sendBasicEmail = async( mailFrom, mailTo, mailSubject, mailBody,) =>{
     try {
         await sender.sendMail({
